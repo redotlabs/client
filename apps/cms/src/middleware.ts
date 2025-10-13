58ac1025ac59c9fef3f3f6e18dd4d5e4150dee82
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-10-14 00:11:10 +0900

    feat(cms): add middleware configuration to exclude specific paths from matching

--- apps/cms/src/middleware.ts
+++ apps/cms/src/middleware.ts
@@ -46,3 +46,16 @@ export async function middleware(request: NextRequest) {
   // On the root domain, allow normal access
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. all root files inside /public (e.g. /favicon.ico)
+     */
+    // '/((?!api|_next|[\\w-]+\\.\\w+).*)',
+    '/((?!_next/static|_next/image|favicon.ico|fonts|images|robots.txt|api|mock).*)',
+  ],
+};
