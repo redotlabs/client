313915856fb9a563015f64c56b062624fbfcf676
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-10-13 17:49:28 +0900

    feat(cms): initialize settings for cms project

--- apps/cms/next.config.ts
+++ apps/cms/next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from 'next';
 
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_DOMAIN || 'http://localhost:3000';
+
 const nextConfig: NextConfig = {
   /* config options here */
   // transpilePackages: ['@repo/ui', '@repo/api-instance'],
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: `${API_BASE_URL}/api/v1/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
