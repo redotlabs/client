02df538790def6dde9dfaaf7cfa75461dd5af96d
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-10-17 15:58:25 +0900

    refactor(cms): remove unused onSubmit function from reset password form

--- apps/cms/src/app/[subdomain]/auth/reset-password/page.tsx
+++ apps/cms/src/app/[subdomain]/auth/reset-password/page.tsx
@@ -39,8 +39,6 @@ export default function ResetPasswordPage() {
     mode: 'onChange',
   });
 
-  const onSubmit = () => {};
-
   return (
     <main className="flex flex-col items-center justify-center h-svh">
       <div className="lg:min-h-[672px] max-w-md w-full px-8 py-10 rounded-4xl shadow-[0_0_18px_0_rgba(0,0,0,0.08)] flex flex-col items-center">
@@ -52,10 +50,7 @@ export default function ResetPasswordPage() {
         <VerifyEmailCodeProvider>
           <ResetPasswordStepProvider>
             <FormProvider {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="flex flex-col size-full"
-              >
+              <form className="flex flex-col size-full">
                 <StepRenderer />
               </form>
             </FormProvider>
