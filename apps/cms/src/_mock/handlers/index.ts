ca0c4abdd9ece88130dfd38af5cc2fae924c4b01
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-10-17 15:57:59 +0900

    feat(cms): include reset password handlers in mock API

--- apps/cms/src/_mock/handlers/index.ts
+++ apps/cms/src/_mock/handlers/index.ts
@@ -1,4 +1,9 @@
 import signInHandlers from './sign-in';
 import customerHandlers from './customer';
+import resetPasswordHandlers from './reset-password';
 
-export const handlers = [...signInHandlers, ...customerHandlers];
+export const handlers = [
+  ...signInHandlers,
+  ...customerHandlers,
+  ...resetPasswordHandlers,
+];
