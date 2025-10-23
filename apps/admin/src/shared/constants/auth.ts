d4488aa382b436c9aa6cce7c67180ee687b528da
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-10-23 16:42:08 +0900

    feat(admin): add constants for authentication, environment variables, and storage keys

---
+++ apps/admin/src/shared/constants/auth.ts
@@ -0,0 +1,3 @@
+import { PATH } from '@/shared/routes';
+
+export const AUTH_WHITE_LIST = [PATH.auth.signIn, PATH.auth.resetPassword];
---
+++ apps/admin/src/shared/constants/storage-keys.ts
@@ -0,0 +1,4 @@
+export const STORAGE_KEYS = {
+  accessToken: 'redot_admin_access_token',
+  refreshToken: 'redot_admin_refresh_token',
+};
