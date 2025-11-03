ac4e99a2a62a1ebfe2363208500e12c8ff1d2e27
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-11-03 18:57:56 +0900

    feat(landing): add shared ui components

---
+++ apps/landing/src/shared/components/layout/footer.tsx
@@ -0,0 +1,25 @@
+import { Logo } from '@redotlabs/ui';
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="px-10 lg:px-20 py-12 flex flex-col bg-primary-500 text-white">
+      <div>
+        <Logo className="w-24" color="mono" />
+      </div>
+      <div className="mt-16 flex flex-col md:flex-row md:items-center justify-between gap-6">
+        <ul className="text-sm font-semibold">
+          <li>사업자등록번호: 123-45-67890 | 대표: 박도륜</li>
+          <li>주소: 서울특별시 송파구 송파동 14-18, 4층</li>
+          <li>연락처: contact@redot.me</li>
+        </ul>
+        <ul className="text-sm">
+          <li>이용약관 | 개인정보처리방침</li>
+          <li>© {new Date().getFullYear()} Redot. All rights reserved.</li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ apps/landing/src/shared/components/layout/header.tsx
@@ -0,0 +1,28 @@
+import { PATH } from '@/shared/constants/routes';
+import { Button, Logo } from '@redotlabs/ui';
+import Link from 'next/link';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-10 bg-white px-8 lg:px-20 h-12.5 lg:h-17.5 flex items-center justify-between border-b border-gray-200">
+      <Link href={PATH.root}>
+        <Logo className="w-auto h-6" />
+      </Link>
+      <div className="flex items-center gap-6">
+        <Link href="https://tech.redot.me" target="_blank">
+          <Button variant="text" className="p-0 text-base">
+            Blog
+          </Button>
+        </Link>
+        <Link href="https://brand.redot.me" target="_blank">
+          <Button variant="text" className="p-0 text-base">
+            Brand
+          </Button>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
