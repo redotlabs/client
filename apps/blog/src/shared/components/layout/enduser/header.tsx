7569eff4fa5c0706abc921a18a6480ea73d6284e
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-11-04 15:40:10 +0900

    style(blog): update AdminHeader and EnduserHeader for improved layout and styling

--- apps/blog/src/shared/components/layout/enduser/header.tsx
+++ apps/blog/src/shared/components/layout/enduser/header.tsx
@@ -5,9 +5,10 @@ import React from 'react';
 
 const EnduserHeader = () => {
   return (
-    <header className="p-4 flex items-center justify-between border-b border-gray-200">
-      <Link href={PATH.root}>
-        <Logo type="logo" className="w-24" />
+    <header className="sticky top-0 z-10 bg-white px-8 lg:px-20 h-12.5 lg:h-17.5 flex items-center justify-between border-b border-gray-200 shrink-0">
+      <Link href={PATH.root} className="flex items-center gap-2">
+        <Logo className="w-auto h-6" />
+        <span className="font-medium">Blog</span>
       </Link>
     </header>
   );
