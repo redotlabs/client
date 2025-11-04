9c60cf473b1a28962df35f814e0cd0ad93788543
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-11-04 23:11:26 +0900

    feat(admin): add DashboardPage component

---
+++ apps/admin/src/pages/dashboard/index.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <main className="p-10 container mx-auto">
+      <h1 className="text-2xl font-bold">대시보드</h1>
+    </main>
+  );
+};
+
+export default DashboardPage;
