2563f3b9c1ab3008637712db882dba5392317feb
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-11-04 23:10:18 +0900

    feat(admin): add AdminLayout component

---
+++ apps/admin/src/shared/components/layouts/admin-layout.tsx
@@ -0,0 +1,20 @@
+import Sidebar from './sidebar';
+import { Outlet } from 'react-router-dom';
+import AuthGuard from '@/shared/components/wrapper/auth-guard';
+import Header from './header';
+
+const AdminLayout = () => {
+  return (
+    <AuthGuard>
+      <div className="size-full flex min-h-svh">
+        <Sidebar />
+        <div className="flex-1 flex flex-col">
+          <Header />
+          <Outlet />
+        </div>
+      </div>
+    </AuthGuard>
+  );
+};
+
+export default AdminLayout;
