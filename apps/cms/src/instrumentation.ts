3c5fff21fb79773f6d0399e52ffa543dea1816f7
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-11-06 20:43:58 +0900

    chore(cms): remove instrumentation.ts file for disable mocking

--- apps/cms/src/instrumentation.ts
+++
@@ -1,7 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const { server } = await import('./_mock/node');
-    server.listen();
-    console.log('[MSW] Server-side mocking enabled');
-  }
-}
