de1ee0ddcf5511c58f04d3824fbeb28bcafc73ee
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-11-24 21:34:37 +0900

    fix(footer): update business registration number and contact email in footer component

--- apps/landing/src/shared/components/layout/footer.tsx
+++ apps/landing/src/shared/components/layout/footer.tsx
@@ -9,9 +9,9 @@ const Footer = () => {
       </div>
       <div className="mt-16 flex flex-col md:flex-row md:items-center justify-between gap-6">
         <ul className="text-sm font-semibold">
-          <li>사업자등록번호: 123-45-67890 | 대표: 박도륜</li>
+          <li>사업자등록번호: 337-66-00719 | 대표: 박도륜</li>
           <li>주소: 서울특별시 송파구 송파동 14-18, 4층</li>
-          <li>연락처: contact@redot.me</li>
+          <li>연락처: team@redot.me</li>
         </ul>
         <ul className="text-sm">
           <li>이용약관 | 개인정보처리방침</li>
