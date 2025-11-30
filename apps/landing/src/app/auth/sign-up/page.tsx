bc727ea622e456e7d90d0e9a748fb354eb7363ae
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-12-01 04:57:21 +0900

    feat(landing): add sign-up process with multi-step form and email verification

--- apps/landing/src/app/auth/sign-up/page.tsx
+++ apps/landing/src/app/auth/sign-up/page.tsx
@@ -1,28 +1,60 @@
-import SignUpForm from './ui/sign-up-form';
-import Link from 'next/link';
+'use client';
+
+import z from 'zod';
+import { FormProvider, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { VerifyEmailCodeProvider } from './hooks/verify-email-code.hook';
+import { SignUpStepProvider } from './hooks/sign-up-step.hook';
+import StepRenderer from './ui/step-renderer';
+
+const schema = z
+  .object({
+    email: z
+      .email('이메일 형식이 일치하지 않습니다.')
+      .min(1, '이메일을 입력해주세요.'),
+    code: z.string().min(6, '인증코드를 6자리를 입력해주세요.'),
+    password: z
+      .string()
+      .regex(
+        /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/,
+        '비밀번호는 영문, 숫자, 특수문자를 포함한 8자리 이상이어야 합니다.'
+      ),
+    passwordConfirm: z.string().min(1, '비밀번호를 입력해주세요.'),
+  })
+  .superRefine((data, ctx) => {
+    const { password, passwordConfirm } = data;
+    if (password !== passwordConfirm) {
+      ctx.addIssue({
+        code: 'custom',
+        message: '비밀번호가 일치하지 않습니다.',
+        path: ['passwordConfirm'],
+      });
+    }
+  });
 
 export default function SignUpPage() {
+  const form = useForm({
+    resolver: zodResolver(schema),
+    mode: 'onChange',
+  });
+
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
+    <main className="flex flex-col items-center justify-center min-h-svh">
       <div className="lg:min-h-[672px] max-w-md w-full px-8 py-10 flex flex-col items-center">
         <h1 className="mt-9 text-3xl font-bold text-primary-800">회원가입</h1>
         <p className="mt-2 text-sm text-gray-600">
           첫 번째 서비스는 무료로 시작하세요
         </p>
 
-        <SignUpForm />
-
-        <div className="mt-6 text-center">
-          <p className="text-sm text-gray-600">
-            이미 계정이 있으신가요?{' '}
-            <Link
-              href="/auth/sign-in"
-              className="text-primary-500 font-semibold hover:underline"
-            >
-              로그인
-            </Link>
-          </p>
-        </div>
+        <VerifyEmailCodeProvider>
+          <SignUpStepProvider>
+            <FormProvider {...form}>
+              <form className="flex flex-col size-full gap-4">
+                <StepRenderer />
+              </form>
+            </FormProvider>
+          </SignUpStepProvider>
+        </VerifyEmailCodeProvider>
       </div>
     </main>
   );
---
+++ apps/landing/src/app/auth/sign-up/ui/success-step.tsx
@@ -0,0 +1,36 @@
+import { PATH } from '@/shared/constants/routes';
+import { Button } from '@redotlabs/ui';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const SuccessStep = () => {
+  const noCache = Date.now();
+
+  return (
+    <>
+      <div className="size-full flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-9">
+          <Image
+            src={`/assets/success.gif?${noCache}`}
+            alt="success"
+            width={160}
+            height={160}
+            className="size-32"
+          />
+
+          <p className="text-center text-gray-800">
+            회원가입이 완료되었어요.
+            <br />
+            로그인 페이지로 이동하여 로그인해주세요.
+          </p>
+        </div>
+      </div>
+
+      <Link href={PATH.auth.signIn} replace className="mt-5 w-full">
+        <Button className="w-full">로그인 페이지로 이동하기</Button>
+      </Link>
+    </>
+  );
+};
+
+export default SuccessStep;
