c3536cd8a43d24da571f9c2074ee346ccb92d423
redot-dev <team@redot.me>
2025-12-02 01:51:40 +0900

    feat(landing): add terms and privacy paths to AUTH_WHITE_LIST

--- apps/landing/src/shared/constants/auth.ts
+++ apps/landing/src/shared/constants/auth.ts
@@ -1,3 +1,9 @@
 import { PATH } from './routes';
 
-export const AUTH_WHITE_LIST = [PATH.root, PATH.auth.signIn, PATH.auth.signUp];
+export const AUTH_WHITE_LIST = [
+  PATH.root,
+  PATH.terms,
+  PATH.privacy,
+  PATH.auth.signIn,
+  PATH.auth.signUp,
+];
