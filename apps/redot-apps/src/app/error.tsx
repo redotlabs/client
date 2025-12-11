a41183c965bf72b263591015c4e7da4487c32814
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-12-11 20:39:05 +0900

    feat(redot-apps): add error, loading, 404 pages

---
+++ apps/redot-apps/src/app/error.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { ThemeProvider } from '@redotlabs/themes';
+import { Button } from '@redotlabs/ui';
+import { Corn } from '@repo/assets';
+
+export default function Error({ reset }: { reset: () => void }) {
+  return (
+    <ThemeProvider color="blue" font="pretendard">
+      <div className="flex items-center justify-center min-h-svh">
+        <main className="flex flex-col items-center">
+          <Corn className="w-[200px] h-auto" />
+
+          <h1 className="text-center flex flex-col gap-2">
+            <span className="text-5xl font-bold">OOPS!</span>
+            <span className="text-3xl font-bold">연결에 문제가 생겼어요</span>
+          </h1>
+
+          <Button className="mt-8" onClick={reset}>
+            새로고침하기
+          </Button>
+        </main>
+      </div>
+    </ThemeProvider>
+  );
+}
