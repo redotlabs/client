d678ad689ea1c91210ddffbbf3b3cee525eb1615
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-12-11 20:39:51 +0900

    fix(redot-apps): implement ClientRenderer component

--- apps/redot-apps/src/app/s/[subdomain]/[[...slug]]/page.tsx
+++ apps/redot-apps/src/app/s/[subdomain]/[[...slug]]/page.tsx
@@ -1,7 +1,8 @@
-import { PageContent, PageRenderer } from '@repo/builder/renderer';
+import { PageContent } from '@repo/builder/renderer';
 import { getPage } from '@/shared/api/services/app';
 import { safeParseJson } from '@repo/utils';
 import { notFound } from 'next/navigation';
+import ClientRenderer from './_ui/client-renderer';
 
 export default async function Page({
   params,
@@ -16,5 +17,5 @@ export default async function Page({
     return notFound();
   }
 
-  return <PageRenderer content={content} />;
+  return <ClientRenderer content={content} />;
 }
