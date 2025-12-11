eba1e4278f9d2be2911df2e37488eff9879856fd
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-12-12 00:25:29 +0900

    feat(cms): add IS_PRODUCTION environment variable and update getBuilderUrl for protocol handling

--- apps/cms/src/shared/utils/get-services-url.ts
+++ apps/cms/src/shared/utils/get-services-url.ts
@@ -1,11 +1,6 @@
-import { ROOT_DOMAIN } from '@/shared/constants/env-variables';
-
-export const getCmsUrl = (subdomain: string) => {
-  const { protocol } = window.location;
-  return `${protocol}//${subdomain}.${ROOT_DOMAIN}/cms`;
-};
+import { IS_PRODUCTION, ROOT_DOMAIN } from '@/shared/constants/env-variables';
 
 export const getBuilderUrl = (subdomain: string) => {
-  const { protocol } = window.location;
-  return `${protocol}//${subdomain}.${ROOT_DOMAIN}/builder`;
+  const protocol = IS_PRODUCTION ? 'https' : 'http';
+  return `${protocol}://${subdomain}.${ROOT_DOMAIN}/builder`;
 };
