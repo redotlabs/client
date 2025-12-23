38a7582d4ce532a9ac1638371450af6ab1ec170e
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-12-23 15:13:20 +0900

    feat(landing): update process section titles and descriptions

--- apps/landing/src/app/_sections/process-section.tsx
+++ apps/landing/src/app/_sections/process-section.tsx
@@ -21,39 +21,34 @@ const ProcessSection = () => {
   return (
     <section className="p-8 md:p-20 bg-primary-200 flex flex-col gap-10">
       <h2 className="text-gray-600 text-2xl lg:text-4xl font-medium">
-        낡은 홈페이지부터 신규 제작까지 미래의 모습으로.
+        Redot은 어떤 서비스를 제공하나요?
       </h2>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
         <ProcessCard
-          title="AI 데이터 분석"
+          title="홈페이지 리뉴얼, 신규제작"
           description={
             <>
-              성공적인 웹사이트는 데이터에서 시작됩니다.
-              <br />
-              경쟁사 분석, 타겟 고객 분석, SEO 전략까지 AI가 최적의 방향을
-              제시합니다
+              5년 이상 된 홈페이지가 있거나, 홈페이지 신규제작이 필요하다면
+              Redot에서 최적의 디자인과 합리적인 유지비를 경험할 수 있어요.
             </>
           }
         />
         <ProcessCard
-          title="전문가 맞춤 제작"
+          title="데이터 가공 및 자동화 툴 제작"
           description={
             <>
-              AI의 분석을 바탕으로
-              <br />
-              숙련된 기획자, 디자이너, 개발자 팀이 브랜드 가치를 극대화하는
-              맞춤형 웹사이트를 제작합니다.
+              대량의 내부 데이터 가공과 자동화 툴을 제공해드려요. 맞춤형 툴을
+              도입하여 압도적인 생산성을 경험해 보세요.
             </>
           }
         />
         <ProcessCard
-          title="간편한 CMS"
+          title="보안 점검 및 컨설팅"
           description={
             <>
-              제작 후에도 개발자 없이 직접 콘텐츠를 관리하세요.
-              <br />
-              워드처럼 쉬운 관리자 페이지(CMS)로 지속적인 성장을 돕습니다.
+              최근 화두인 보안 이슈에 대해 보안 점검 및 컨설팅을 진행하고 더욱
+              안전한 서비스가 될 수 있도록 도와드려요.
             </>
           }
         />
